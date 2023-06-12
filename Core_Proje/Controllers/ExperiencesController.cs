f0e4d79acfbd22756bbc8104358e692ff69e0fb8
Şeyma YILMAZER <seymaesim@users.noreply.github.com>
2023-06-12 17:13:02 +0300

    Deneyimler alanına başlandı

---
+++ Core_Proje/Controllers/ExperiencesController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class ExperiencesController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDAL());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Listeleme";
+            ViewBag.v2 = "Deneyim";
+            ViewBag.v3 = "Deneyim Listesi";
+            var experienceList = experienceManager.T_GetList();
+            return View(experienceList);
+         
+        }
+    }
+}
